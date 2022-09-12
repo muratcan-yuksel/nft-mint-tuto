55289bd3d8c5f4a4ad4c4564be902f7719773e70
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-13 01:32:37 +0300

    .

--- contracts/RoboPunksNFT.sol
+++ contracts/RoboPunksNFT.sol
@@ -4,3 +4,32 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RoboPunksNFT is ERC721, Ownable {
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    uint256 public maxPerWallet;
+    //the owner can toggle this to allow minting
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+    //we're not gonna code withdraw functionality
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+
+    constructor() payable ERC721("RoboPunks", "RP") {
+        mintPrice = 0.02 ether;
+        //we start with a zero
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+        //set withdraw wallet address here
+        //he doesn't do it, you have to
+    }
+
+    //owner is set as msg.sender by default by openzeppelin's Ownable.sol contract
+    function setIsPublicMintEnabled(bool _isPublidMintEnabled)
+        external
+        onlyOwner
+    {}
+}
