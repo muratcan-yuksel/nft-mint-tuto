f7a7c473476a3f9b69710aff3929611799fcd48f
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-14 02:17:16 +0300

    forgot to add chakra to index.js

--- src/components/MainMint.js
+++ src/components/MainMint.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ethers, BigNumber } from "ethers";
 import roboPunksNFT from "../contract/RoboPunksNFT.json";
+import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
 
 //the address of the deployed contract
 const roboPunksNFTAddress = "0xc224dfb26fbB41f703d7dF4146aB99ffC776Fa15";
@@ -40,25 +41,96 @@ const MainMint = ({ accounts, setAccounts }) => {
     setMintAmount(mintAmount + 1);
   };
   return (
-    <div>
-      <h1>RoboPunks</h1>
-      <p>
-        It's 2078. Can the RoboPunks NFT save humans from destructive rampant
-        NFT speculation? Mint Robopunks to find out.
-      </p>
-      {isConnected ? (
+    <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
+      <Box width="520px">
         <div>
-          <div>
-            <button onClick={handleDecrement}>- </button>
-            <input type="number" value={mintAmount} />
-            <button onClick={handleIncrement}>+ </button>
-          </div>
-          <button onClick={handleMint}>Mint Now</button>
+          <Text fontSize="48px" textShadow="0 5px #000000">
+            RoboPunksNFT
+          </Text>
+          <Text
+            fontSize="30px"
+            letterSpacing="-5.5%"
+            fontFamily="VT323"
+            textShadow="0 2px 2px #000000"
+          >
+            This website is for demo purposes only.
+          </Text>
         </div>
-      ) : (
-        <p>Connect your wallet to mint</p>
-      )}
-    </div>
+
+        {isConnected ? (
+          <div>
+            <Flex justify="center" align="center">
+              <Button
+                backgroundColor="#008fd4"
+                borderRadius="5px"
+                boxShadow="0px 2px 2px 1px #0F0F0F"
+                color="white"
+                cursor="pointer"
+                fontFamily="inherit"
+                padding="15px"
+                margin="10"
+                onClick={handleDecrement}
+              >
+                {" "}
+                -
+              </Button>
+
+              <Input
+                readOnly
+                fontFamily="inherit"
+                width="100px"
+                height="40px"
+                textAlign="center"
+                paddingLeft="19px"
+                marginTop="10px"
+                type="number"
+                value={mintAmount}
+              />
+
+              <Button
+                backgroundColor="#008fd4"
+                borderRadius="5px"
+                boxShadow="0px 2px 2px 1px #0F0F0F"
+                color="white"
+                cursor="pointer"
+                fontFamily="inherit"
+                padding="15px"
+                margin="10"
+                onClick={handleIncrement}
+              >
+                {" "}
+                +
+              </Button>
+            </Flex>
+
+            <Button
+              backgroundColor="#008fd4"
+              borderRadius="5px"
+              boxShadow="0px 2px 2px 1px #0F0F0F"
+              color="white"
+              cursor="pointer"
+              fontFamily="inherit"
+              padding="15px"
+              margin="10"
+              onClick={handleMint}
+            >
+              Mint Now
+            </Button>
+          </div>
+        ) : (
+          <Text
+            marginTop="70px"
+            fontSize="30px"
+            letterSpacing="5.5%"
+            fontFamily="VT323"
+            textShadow="0 3px #000000"
+            color="#008fd4"
+          >
+            Connect your wallet to mint.
+          </Text>
+        )}
+      </Box>
+    </Flex>
   );
 };
 
