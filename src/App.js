c34afc0c2f7bfba4126269019baf43ef4e4e8ca1
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-09-14 01:43:12 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
-import bgImg from "./assets/assets/background/parallax-bg.gif";
+// import bgImg from "./assets/assets/background/parallax-bg.gif";
+import "./App.css";
 import MainMint from "./components/MainMint";
 import NavBar from "./components/NavBar";
-import "./App.css";
 
 const App = () => {
   const [accounts, setAccounts] = useState([]);
+
   return (
-    <div>
-      <NavBar accounts={accounts} setAccounts={setAccounts} />
-      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    <div className="overlay">
+      <div className="App">
+        <div className="App">
+          <NavBar accounts={accounts} setAccounts={setAccounts} />
+          <MainMint accounts={accounts} setAccounts={setAccounts} />
+        </div>
+        <div className="moving-background"> </div>
+      </div>
     </div>
   );
 };
